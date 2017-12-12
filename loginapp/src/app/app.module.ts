3869d2f277cdcc183f1a9ef12f276f62a65fa5cd
hsallbd@gmail.com <hsallbd@gmail.com>
2017-12-12 16:06:09 +0530

    Initial Commit

---
+++ loginapp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
